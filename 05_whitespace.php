891baa771dcc227833850816b5b593d3e3fbee72
unknown <james.dalin@laplateforme.io>
2025-07-22 16:45:35 +0200

    ajout du cours 5 et 6

---
+++ 05_whitespace.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Les espace blans seront ignorés, mais sur la page web, les données sont affichées sur la page sans les espaces */
+
+echo "espace blanc"
+?>
---
+++ 06_escape_character.php
@@ -0,0 +1,24 @@
+<!-- 
+ \n ou <br> - Nouvelle ligne 
+ \t - Tab - &nbsp;&nbsp;
+ \$ - Signe du Dollar
+ \" - Double guilleemts
+ \' - Un seul guillemet
+ \\ - Single Backlash - en français, permet de faire les apostrophes-->
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace entre les caractères</title>
+ </head>
+ <body>
+    
+<p><?php echo "Les espaces entre les caractères sont <br>
+\"importants\" dans la structure &nbsp; du contenu dans PHP, et sa vous fera 100 \$ content";?></p>
+
+<p><?php echo 'Je m\'appelle ... Je dois l\'appeler';?></p>
+
+ </body>
+ </html>
